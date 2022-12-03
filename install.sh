17188053bc7157548999cece3524cd0367a4632f
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2022-12-03 16:29:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd /etc
 
 apt-get -y install jq
-https://github.com/lahirubro123/Vps-to-Telegram-Backup-Tool.git
+git clone https://github.com/lahirubro123/Vps-to-Telegram-Backup-Tool.git
 chmod +x /etc/Vps-to-Telegram-Backup-Tool/tgsend.sh
 chmod +x /etc/Vps-to-Telegram-Backup-Tool/backup.sh
 
