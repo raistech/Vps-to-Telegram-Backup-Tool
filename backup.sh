6300c1e33a679eeebd0d237fff12d656e0c9ee39
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2022-12-03 15:46:23 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,5 +2,9 @@
 
 
 # Install  packages
-./tgsend.sh sendf /etc/x-ui/x-ui.db
+#./tgsend.sh sendf /etc/x-ui/x-ui.db
 
+# Commands to Run
+
+/etc/Vps-to-Telegram-Backup-Tool/tgsend.sh send "V2ray Server 03 Backup"
+/etc/Vps-to-Telegram-Backup-Tool/tgsend.sh sendf /etc/x-ui/x-ui.db
