06b3dff2fc3e839860ea61248c53f1457b104750
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2022-12-03 15:41:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 cd /etc
+
 https://github.com/lahirubro123/Vps-to-Telegram-Backup-Tool.git
+chmod +x /etc/Vps-to-Telegram-Backup-Tool/tgsend.sh
 chmod +x /etc/Vps-to-Telegram-Backup-Tool/backup.sh
+
 (crontab -l; echo "0 */12 * * * sudo bash /etc/Vps-to-Telegram-Backup-Tool/backup.sh") | sort -u | crontab -
+
+echo "Setup Done"
